f744b301801bbf5d3796d8fe644f638733e9603b
Lucas <lucas@Lucass-MBP.eeple.fr>
2018-09-18 11:33:34 +0200

    add email required field

--- kisee/utils.py
+++ kisee/utils.py
@@ -12,3 +12,4 @@ async def get_user_with_email_or_username(
     if "email" in user_input:
         return await idp_backend.get_user_by_email(user_input["email"])
     return await idp_backend.get_user_by_username(user_input["login"])
+
