267c2fc1f90a40bfb9046dcdba990fde302be4aa
Lucas <lucas@Lucass-MBP.eeple.fr>
2018-09-21 14:47:51 +0200

    reajust coverage to 100%

--- tests/mocks.py
+++ tests/mocks.py
@@ -7,9 +7,3 @@ def register_user(self, username, password, is_superuser=False):
     """Mocker register user
     """
     raise UserAlreadyExist
-
-
-def send_mail(subject, text, html, email_settings, recipient):
-    """Send mail
-    """
-    pass
